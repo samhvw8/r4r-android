c27fa97abd4e3c2e9a5b238b123046aeb2187469
Sam Hoang <samhv.ict@gmail.com>
2016-05-25 04:01:31 +0700

    Fix: error can view detail when has button

--- app/src/main/java/com/R4RSS/adapters/RoomAdapter.java
+++ app/src/main/java/com/R4RSS/adapters/RoomAdapter.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.R4RSS.GlobalValues;
@@ -16,9 +17,14 @@ import com.R4RSS.models.RoomModel;
 import com.R4RSS.r4r.EditRoom;
 import com.R4RSS.r4r.MainActivity;
 import com.R4RSS.r4r.R;
+import com.R4RSS.r4r.RoomDetail;
 import com.R4RSS.r4r.UserRooms;
 import com.R4RSS.requests.DeleteRoomRequest;
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.nostra13.universalimageloader.core.ImageLoader;
 
@@ -34,6 +40,7 @@ public class RoomAdapter extends ArrayAdapter {
 
     private List<RoomModel> roomAdapterList;
     private int resource;
+    private static final String USER_INFO_URL = "http://52.36.12.106/api/v1/user/";
 
     public RoomAdapter(Context context, int resource, List<RoomModel> objects) {
         super(context, resource, objects);
@@ -62,7 +69,7 @@ public class RoomAdapter extends ArrayAdapter {
         final TextView tvPrice;
         TextView Address;
         TextView tvTimestamp;
-
+        LinearLayout layout;
         ImageView delete, edit;
 
         imgRoom = (ImageView) convertView.findViewById(R.id.imgRoom);
@@ -71,6 +78,7 @@ public class RoomAdapter extends ArrayAdapter {
         tvTimestamp = (TextView) convertView.findViewById(R.id.tvTimestamp);
         delete = (ImageView) convertView.findViewById(R.id.delete_room);
         edit = (ImageView) convertView.findViewById(R.id.edit_room);
+        layout = (LinearLayout) convertView.findViewById(R.id.layout);
         //tvDescriptionRoom = (TextView) convertView.findViewById(R.id.tvDescriptionRoom);
 
         ImageLoader.getInstance().displayImage(roomAdapterList.get(position).getImage_album_url(), imgRoom); // Default options will be used
@@ -92,7 +100,7 @@ public class RoomAdapter extends ArrayAdapter {
         final int userId = roomAdapterList.get(position).getUserId();
         final int id = roomAdapterList.get(position).getId();
 
-
+        final int p = position;
 
         if (GlobalValues.getId() == userId) {/////
             delete.setVisibility(View.VISIBLE);
@@ -101,6 +109,80 @@ public class RoomAdapter extends ArrayAdapter {
             delete.setVisibility(View.INVISIBLE);
             edit.setVisibility(View.INVISIBLE);
         }
+        layout.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                //get dia chi vao activity RoomDetail
+                String street = roomAdapterList.get(p).getStreet();
+                String ward = roomAdapterList.get(p).getWard();
+                String district = roomAdapterList.get(p).getDistrict();
+                String city = roomAdapterList.get(p).getCity();
+                final String address = street + "-" + ward + "-" + district + "-" + city + "\n";
+                final String price = Integer.toString(roomAdapterList.get(p).getPrice()) + "VND\n";
+                final String area = Double.toString(roomAdapterList.get(p).getArea()) + "m2\n";
+                final String description = roomAdapterList.get(p).getDescription() + "\n";
+                final String userId = Integer.toString(roomAdapterList.get(p).getUserId());
+                //get toa do vao activity RoomDetail
+//                String lat = Double.toString(room.getLatitude());
+//                String lng = Double.toString(room.getLongtitude());
+                final double lat = roomAdapterList.get(p).getLatitude();
+                final double lng = roomAdapterList.get(p).getLongtitude();
+
+
+                final RequestQueue queue = Volley.newRequestQueue(getContext());
+                JsonObjectRequest getRequest = new JsonObjectRequest(Request.Method.GET, USER_INFO_URL + userId, null,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                // display response
+                                Log.d("Response", response.toString());
+                                //save info into share preference to later use
+                                boolean status = Boolean.parseBoolean(response.opt("status").toString());
+                                if (status) {
+
+                                    String day;
+                                    int id;
+                                    try {
+                                        JSONObject data = response.getJSONObject("data");
+                                        JSONObject user = data.getJSONObject("user");
+
+                                        String phone = user.getString("phone");
+                                        String email = user.getString("email");
+                                        String contact = "Email : "+ email +"\n"+ "Phone : " + phone;
+
+                                        Intent intent = new Intent(getContext(), RoomDetail.class);
+                                        intent.putExtra("price", price);
+                                        intent.putExtra("address", address);
+                                        intent.putExtra("area", area);
+                                        intent.putExtra("description", description);
+                                        intent.putExtra("lat", lat);
+                                        intent.putExtra("lng", lng);
+                                        intent.putExtra("userId", userId);
+                                        intent.putExtra("contact", contact);
+
+                                        getContext().startActivity(intent);
+
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Log.d("Error.Response", error.toString());
+                            }
+                        }
+                ) {
+                };
+
+                // add it to the RequestQueue
+                queue.add(getRequest);
+            }
+        });
+
 
         delete.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -186,6 +268,8 @@ public class RoomAdapter extends ArrayAdapter {
 //                queue.add(request);
             }
         });
+
+
 
         return convertView;
     }
